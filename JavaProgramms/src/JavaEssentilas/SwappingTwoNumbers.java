78c3417cee6498fa4d342af1895910a1df798b19
surajdarekar95 <darekarsuraj10395@gmail.com>
2021-10-30 23:17:57 +0530

    Java Essential programms

---
+++ JavaProgramms/src/JavaEssentilas/SwappingTwoNumbers.java
@@ -0,0 +1,23 @@
+package JavaEssentilas;
+
+public class SwappingTwoNumbers {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=20;
+		
+		System.out.println("Before swapping..."+a+" " +b);
+		
+		//Logic 1
+		
+		int t=a;
+		a=b;
+		b=t;
+		
+		System.out.println("After swapping..."+a+" " +b);
+
+		
+	}
+
+}
